17906d9f3cf69e8fb4717bc60e4e3a690d881385
wuzhixuan <wuzhixuan@qq.com>
2020-05-22 14:57:28 +0800

    Initial commit

---
+++ src/main/java/com/study/learn_log/LearnLogApplication.java
@@ -0,0 +1,13 @@
+package com.study.learn_log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnLogApplication.class, args);
+    }
+
+}
